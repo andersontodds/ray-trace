ebed1d1dd17cfddccd3b0818608d4f5709995547
todd <andersontodds@gmail.com>
2020-10-23 16:05:34 -0700

    v0.5: incorrect ray-tracing, try w/ possible error in Py version

--- scratch.jl
+++ scratch.jl
@@ -45,7 +45,8 @@
 	5. Run and time solver, optimize
 =#
 #const rₑ = 6378100
-re = 6.3781e6      	# radius of Earth in meters
+c = 2.99792458e8
+re = 6.3712e6      	# radius of Earth in meters
 B0 = 3.0696381e-5   # magnitude of B field (where?) in Tesla
 e = 1.602e-19 		# electric charge in Coulombs
 me = 9.1093e-31		# electron rest mass
@@ -125,12 +126,40 @@ u = function phase_refractive_index(r,θ,χ,freq)
 	F = sqrt(F2)
 
 	μ2_minus = (B - F)/(2.0*A)
-	μ_minus = sqrt(μ2_minus)
 	μ2_plus = (B + F)/(2.0*A)
-	μ_plus = sqrt(μ2_plus)
 
-	# Electron whistler case: ψ = 0, μ² = R; this is the μ_minus case
-	μ = μ_minus
+	μ_minus = try
+		sqrt(μ2_minus)
+
+	catch err
+		if isa(err,DomainError)
+			println("DomainError in μ_minus!")
+			println("B=", B)
+			println("F=", F)
+		else
+			println(err)
+		end
+	end
+
+	μ_plus = try
+		sqrt(μ2_plus)
+
+	catch err
+		if isa(err,DomainError)
+			println("DomainError in μ_plus!")
+			println("B=", B)
+			println("F=", F)
+			println("A=", A)
+		else
+			println(err)
+		end
+	end
+
+
+
+	# Electron whistler case: ψ = 0, μ² = R; this is the μ_plus case
+	μ = μ_minus		# EMIC case
+	#μ = μ_plus		# electron whistler case
 
 	# Find dA/dψ, dB/dψ, dC/dψ, dμ/dψ
 	dAdψ = 2.0*(S-P)*sin(ψ)*cos(ψ)
@@ -201,14 +230,14 @@ end
 
 # calculate derivatives w.r.t. time
 function haselgrove!(du,u,p,t)
-# 	du = drdt, dθdt, dχdt
-# 	u = r, θ, χ
+# 	du = drdt, dθdt, dχdt, dfdt
+# 	u = r, θ, χ, f
 #  	p = freq, μ, dμdψ, dμdr, dμdθ, dμdχ, dμdf
 # 		^ this compiles and runs, but parameters
 # 	t = time
 
-	r, θ, χ = u 						# unpack u
-	freq = p[1] 	# unpack p
+	r, θ, χ, freq = u 						# unpack u
+	#freq = p[1] 	# unpack p
 
 	dμdr = ddr(r,θ,χ,freq)			# mutating form: ddr!([μ,dμdψ],r,θ,χ,freq,dμdr)
 	dμdθ = ddθ(r,θ,χ,freq)			# calls phase_refractive_index(r,θ,χ,freq)
@@ -222,6 +251,7 @@ function haselgrove!(du,u,p,t)
     du[1] = 1/(μ^2)*(μ*cos(χ)+dμdχ*sin(χ))
     du[2] = 1/(r*μ^2)*(μ*sin(χ)-dμdχ*cos(χ))
     du[3] = 1/(r*μ^2)*(dμdθ*cos(χ)-(r*dμdr + μ)*cos(χ))
+	du[4] = 1/c*(1+(freq/μ)*dμdf)
 
 	#du = [drdt, dθdt, dχdt]
 
@@ -250,9 +280,9 @@ end
 #haselgrove!(μ,dμdψ,r,θ,χ,freq,dμ,ddt,t)
 #haselgrove!(1.0,1.0,10000.0,pi/4.0,0.0,5000.0,[1.0,1.0,1.0,1.0],[1.0,1.0,1.0],0.0)
 
-du0 = [1.0, 1.0, 1.0]
-u0 = [8000.0, pi/4.0, 0.0]
-p = [5000.0]
+du0 = [1.0, 1.0, 1.0, 1.0]
+u0 = [8000.0, pi/4.0, 0.0, 5000.0]
+p = []
 t0 = 0.0
 haselgrove!(du0, u0, p, t0)
 println(du0)
@@ -261,16 +291,32 @@ println(du0)
 #QUESTION: how does ODEProblem work?
 
 using BenchmarkTools
+using LSODA
 
-u0 = [8000.0, pi/4.0, 0.0]					# r0, θ0, χ0
-p = [5000.0]	# f0, dμdψ, dμdr, dμdθ, dμdχ, dμdf
-tspan = (0.0,1.0)
+u0 = [re+1.0e+6, -1.0*pi/4, 0.0, 5000.0]					# r0, θ0, χ0
+p = []	# f0, dμdψ, dμdr, dμdθ, dμdχ, dμdf
+tspan = (0.0,1.0e+11)
 
 hasel_prob = ODEProblem(haselgrove!,u0,tspan,p)
-@benchmark hasel_soln = solve(hasel_prob)
+hasel_soln = solve(hasel_prob)
 
 using Plots
 plot(hasel_soln)
+plot(hasel_soln, vars=(1,2,3))
+
+r = hasel_soln[1,:]
+θ = hasel_soln[2,:]
+χ = hasel_soln[3,:]
+f = hasel_soln[4,:]
+
+x = r.*cos.(θ)
+y = r.*sin.(θ)
+
+plot(x,y)
+
+
+#earthx = re/1.0e3.*cos([0:0.01:2*π;])
+plot!(re.*cos.([0:0.01:2*π;]),re.*sin.([0:0.01:2*π;]))
 
 ##
 function lorenz!(du, u, p, t)
@@ -286,11 +332,13 @@ sol2 = solve(prob2)
 
 plot(sol2,vars=(1,2,3))
 
-## try a nested ODE
-function someODE!(du,u,p,t)
-	du
+## B-field check
 
-end
+re = 6.3712e6      	# radius of Earth in meters
+B0 = 3.0696381e-5	# magnetic field strentgh (where?) in Tesla
+r = 5000.0
+θ = pi/4.0
+Bmag = B0*((re^3)/(r^3))*sqrt(4.0-3.0*cos((pi/2.)-θ)*cos((pi/2.0)-θ))
 
 ##
 
@@ -319,19 +367,75 @@ a = [1:5;]
 sum(a)
 
 ## type/value check
-# r = 8000.0
-# θ = pi/4.0
-# χ = 0.0
-# freq = 5000.0
-# dip = atan(2.0*tan(pi/2.0-θ))
-# ϕ = (3.0/2.0)*pi - dip
-# ψ = χ - ϕ
-# ω = 2.0*pi*freq
-# rE = r/re
-# Bmag = B0*((re^3)/(r^3))*sqrt(4.0-3.0*cos((pi/2.)-θ)*cos((pi/2.0)-θ))
-# n_e = 1.e6*(1.8e5*exp(-4.183119*(rE-1.0471)))
-# n_p = 1.e6*(1.8e5*exp(-4.183119*(rE-1.0471)))
-# ω_e2 = (n_e*(e^2))/(eps*me)
-# ω_p2 = (n_p*(e^2))/(eps*mp)
-# Ω_e = (e*Bmag)/(me)
-# Ω_p = (e*Bmag)/(mp)
+r = re + 1.0e6
+θ = pi/4.0
+χ = 0.0
+freq = 5000.0
+dip = atan(2.0*tan(pi/2.0-θ))
+dip*180/pi	# dip angle: angle between horizontal and B field
+ϕ = (3.0/2.0)*pi - dip              # intermediate angle -- NOT azimuth
+ψ = χ - ϕ							# wave normal angle: angle between wave direction and B
+
+# convert from frequency to angular frequency
+ω = 2.0*pi*freq
+
+# convert radius to fraction of Earth radius
+rE = r/re
+re
+
+# find magnetic field at (r,θ) from dipole field model
+B0
+Bmag = B0*((re^3)/(r^3))*sqrt(4.0-3.0*cos((pi/2.0)-θ)*cos((pi/2.0)-θ))
+B0*((re^3)/(r^3))
+sqrt(4.0-3.0*cos((pi/2.0)-θ)*cos((pi/2.0)-θ))
+# calculate electron and proton density profiles
+n_e = 1.e6*(1.8e5*exp(-4.183119*(rE-1.0471)))
+n_p = 1.e6*(1.8e5*exp(-4.183119*(rE-1.0471)))
+
+# electron plasma angular frequency squared
+ω_e2 = (n_e*(e^2))/(eps*me)
+# proton plasma angular frequency squared
+ω_p2 = (n_p*(e^2))/(eps*mp)
+
+# electron angular gyrofrequency
+Ω_e = (e*Bmag)/(me)
+# proton angular gyrofrequency
+Ω_p = (e*Bmag)/(mp)
+
+# define R, L, P, D, S,
+# R ≡ 1 - Σ(ωₖ²/ω²)(ω/(ω+ϵₖΩₖ))
+R = 1.0 - (ω_e2/ω^2.0)*(ω/(ω - Ω_e)) - (ω_p2/ω^2.0)*(ω/(ω + Ω_p))
+#println("R = ",R)
+
+# L ≡ 1 - Σ(ωₖ²/ω²)(ω/(ω-ϵₖΩₖ))
+L = 1.0 - (ω_e2/ω^2.0)*(ω/(ω + Ω_e)) - (ω_p2/ω^2.0)*(ω/(ω - Ω_p))
+#println("L = ",L)
+
+# P ≡ 1 - Σ(ωₖ²/ω²)
+P = 1.0 - (ω_e2/ω^2.0) - (ω_p2/ω^2.0)
+#println("P = ",P)
+
+# D ≡ ½(R-L); S ≡ ½(R+L)
+D = (R - L)/2.0
+S = (R + L)/2.0
+
+# define parts of dispersion relation
+# Aμ⁴ - Bμ² + C = 0
+# A = Ssin²ψ + Pcos²ψ
+A = S*sin(ψ)^2.0 + P*cos(ψ)^2.0
+
+# B = RLsin²ψ + PS(1 + cos²ψ)
+B = R*L*sin(ψ)^2.0 + P*S*(1.0+cos(ψ)^2.0)
+
+# C  = PRL
+C = P*R*L
+
+# solve the dispersion relation for μ:
+# μ² = (B +- F)/2A
+# where F² = (RL-PS)²sin⁴ψ + 4P²D²cos²ψ
+#QUESTION: is it faster to (a) solve for F², then sqrt(); or (b) solve for F directly? (a) requires fewer sqrt() calls
+F2 = (R*L - P*S)^2.0*sin(ψ)^4.0 + 4.0*(P*D*cos(ψ))^2.0
+F = sqrt(F2)
+
+μ2_minus = (B - F)/(2.0*A)
+μ2_plus = (B + F)/(2.0*A)
